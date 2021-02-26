f0a2e06344242cd262e7ae1389edd4615abd38e2
Mark Mesich <mmesich@nvidia.com>
2021-02-26 16:09:27 -0600

    Updated example source

--- source/languages/cuda-cpp/examples/#001.cu
+++ source/languages/cuda-cpp/examples/#001.cu
@@ -1,18 +1,31 @@
-// Example of:
-//  __global__
-//  threadIdx
-
 // Kernel definition
-__global__ void VecAdd(float* A, float* B, float* C)
-{
-    int i = threadIdx.x;
-    C[i] = A[i] + B[i];
-}
+__global__ void vectorAdd(const float *A, const float *B, float *C, int numElements)
+ {
+     int i = blockDim.x * blockIdx.x + threadIdx.x;
 
-int main()
-{
-    // ...
-    // Kernel invocation with N threads
-    VecAdd<<<1, N>>>(A, B, C);
-    // ...
-}
+     if (i < numElements)
+     {
+         C[i] = A[i] + B[i];
+     }
+ }
+
+ int main(void)
+ {
+     // Initialize some stuff...
+     float *d_A = NULL;
+     float *d_B = NULL;
+     float *d_C = NULL;
+
+     // Do some stuff...
+
+     // Launch the kernel
+     int numElements = 50000;
+     int threadsPerBlock = 256;
+     int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
+
+     vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
+
+     // Clean up...
+
+     return 0;
+ }
